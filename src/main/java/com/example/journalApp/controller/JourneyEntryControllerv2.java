72d945cef722e72f0e943d19fce291dd8db6999e
Ayush Bansal <ayushbansal095@gmail.com>
2024-10-15 10:45:07 +0530

    rest api with mongodb connection

---
+++ src/main/java/com/example/journalApp/controller/JourneyEntryControllerv2.java
@@ -0,0 +1,32 @@
+package com.example.journalApp.controller;
+
+import com.example.journalApp.entity.JournalEntry;
+import com.example.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RequestMapping("/journal")
+@RestController
+public class JourneyEntryControllerv2 {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return journalEntryService.getAll();
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry newEntry) {
+        journalEntryService.saveEntry(newEntry);
+        return true;
+    }
+
+    @GetMapping("/id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId){
+        return journalEntryService.findById(myId).orElse(null);
+    }
+}
